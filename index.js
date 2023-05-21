7ad913c3ff0bd247bb19627903f792ff5cf810d9
Asif Hossain <asifh9277@gmail.com>
2023-05-22 02:23:43 +0600

    issues

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ function verifyJWT(req, res, next) {
 //?----> middlewares end
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f7eznot.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
